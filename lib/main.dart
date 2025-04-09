7e43da275ade6b8ef553023e49ad036f2d6964e4
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-04-09 23:50:19 +0600

    scaffold, leading, actions

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Live', home: Home());
+    return MaterialApp(
+      title: 'Flutter Live',
+      debugShowCheckedModeBanner: true,
+      home: Home(),
+    );
   }
 }
 
@@ -19,12 +23,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.grey.shade300,
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          'Home Screen',
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: Icon(Icons.home_filled, color: Colors.white, size: 30),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Money Has been transferred'),
+                  duration: Duration(seconds: 0),
+                  backgroundColor: Colors.green,
+                ),
+              );
+            },
+            icon: (Icon(Icons.messenger, color: Colors.white, size: 30)),
+          ),
+        ],
+      ),
       body: Center(
-        child: Text('Hello'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [Text('Hello Word'), Text("I'm Nahid")],
+        ),
       ),
     );
   }
 }
 
-//47
+//34

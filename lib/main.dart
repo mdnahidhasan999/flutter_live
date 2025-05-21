1a2283f879d29429507fff3221d8fa8ecb60d2a4
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-05-21 23:11:47 +0600

    scaffold, leading, actions

--- lib/main.dart
+++ lib/main.dart
@@ -21,30 +21,69 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.green,
       body: Center(
-        // child: Text(
-        //   'Hello, Flutter!, Welcome to Flutter Live',
-        //   textAlign: TextAlign.center,
-        //   maxLines: 1,
-        //   overflow: TextOverflow.ellipsis,
-        //   style: TextStyle(
-        //     fontSize: 24,
-        //     fontWeight: FontWeight.bold,
-        //     color: Colors.white,
-        //     backgroundColor: Colors.purple,
-        //   ),
-        // ),
-        // child: Icon(Icons.android, size: 84, color: Colors.white),
-        child: Image.network('https://www.w3schools.com/howto/img_avatar.png',
-          width: 200,
-          height: 200,
-          fit: BoxFit.cover,
-          color: Colors.white,
-          colorBlendMode: BlendMode.darken,
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  side: BorderSide(color: Colors.white, width: 2),
+                ),
+                textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                padding: EdgeInsets.all(24),
+              ),
+              onPressed: () {
+                print('Elevated Button Clicked');
+              },
+              child: Text('Elevated Button'),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blueGrey,
+                textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
 
+              onPressed: () {},
+              child: Text('Text Button'),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                maxLength: 10,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Enter Text',
+                  hintStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                  label: Text('Label'),
+                  //labelText: 'Email',
+                  //   focusedBorder: OutlineInputBorder(
+                  //     borderRadius: BorderRadius.circular(2),
+                  //     borderSide: BorderSide(width: 50, color: Colors.white),
+                  //   )
+                  // ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-//47
+//41

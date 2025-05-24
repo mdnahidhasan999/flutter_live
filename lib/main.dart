15d53e8b2f0a9bbc1be20e46e4ccb35f1c6762a5
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-05-24 15:03:11 +0600

    Colum, Pixel, SingleChildScrollView,ListView, ListView.builder,ListView itemCount, ListView separated

--- lib/main.dart
+++ lib/main.dart
@@ -14,34 +14,166 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  List<String> studentList = [
+    'Nahid',
+    'Sakib',
+    'Shakib',
+    'Sakib Al Hasan',
+    'Sakib Al Hasan 2',
+
+    'Sakib Al Hasan 3',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Center(
-        // child: Text(
-        //   'Hello, Flutter!, Welcome to Flutter Live',
-        //   textAlign: TextAlign.center,
-        //   maxLines: 1,
-        //   overflow: TextOverflow.ellipsis,
-        //   style: TextStyle(
-        //     fontSize: 24,
-        //     fontWeight: FontWeight.bold,
-        //     color: Colors.white,
-        //     backgroundColor: Colors.purple,
-        //   ),
-        // ),
-        // child: Icon(Icons.android, size: 84, color: Colors.white),
-        child: Image.network('https://www.w3schools.com/howto/img_avatar.png',
-          width: 200,
-          height: 200,
-          fit: BoxFit.cover,
-          color: Colors.white,
-          colorBlendMode: BlendMode.darken,
-        ),
 
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //       Text('Hello World'),
+      //     ],
+      //   ),
+      // ),
+      // body: ListView(
+      //   children: [
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //     Text('Hello World'),
+      //   ],
+      // ),
+      // body: ListView.builder(
+      //   itemCount: 100,
+      //   itemBuilder: (context, index) {
+      //     return Text('${index + 1}', style: TextStyle(fontSize: 32));
+      //   },
+      // ),
+      // body: ListView.builder(
+      //   itemCount: studentList.length,
+      //   itemBuilder: (context, index) {
+      //     return Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Column(
+      //         children: [
+      //           Text(studentList[index],
+      //           style: TextStyle(
+      //             fontSize: 18
+      //           ),),
+      //           Divider(),
+      //         ],
+      //       ),
+      //
+      //     );
+      //   },
+      // ),
+      body: ListView.separated(
+        itemCount: studentList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(studentList[index], style: TextStyle(fontSize: 18)),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(color: Colors.white, thickness: 2, height: 20,
+          endIndent: 16,
+          indent: 16);
+        },
       ),
     );
   }

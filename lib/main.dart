62ad9a774ac3934e074b7b1527c9b96db577e58c
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-08-03 23:41:54 +0600

    cupertino widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,37 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(iOSStyleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class iOSStyleApp extends StatelessWidget {
+  const iOSStyleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Live', home: Home());
+    return const CupertinoApp(home: HomePage());
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
-      body: Center(
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Home'),
+        trailing: Icon(CupertinoIcons.add),
+      ),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  side: BorderSide(color: Colors.white, width: 2),
-                ),
-                textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                padding: EdgeInsets.all(24),
-              ),
-              onPressed: () {
-                print('Elevated Button Clicked');
-              },
-              child: Text('Elevated Button'),
-            ),
-            SizedBox(height: 20),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.blueGrey,
-                textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-
-              onPressed: () {},
-              child: Text('Text Button'),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                maxLength: 10,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                decoration: InputDecoration(
-                  hintText: 'Enter Text',
-                  hintStyle: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
-                  ),
-                  label: Text('Label'),
-                  //labelText: 'Email',
-                  //   focusedBorder: OutlineInputBorder(
-                  //     borderRadius: BorderRadius.circular(2),
-                  //     borderSide: BorderSide(width: 50, color: Colors.white),
-                  //   )
-                  // ),
-                ),
-              ),
-            ),
+            const CupertinoActivityIndicator(radius: 25),
+            CupertinoSwitch(value: false, onChanged: (value) {}),
           ],
         ),
       ),
     );
   }
 }
-
-//41
